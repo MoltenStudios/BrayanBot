0b45148283101ee8da816cd71572ae375321a94d
DakshNamdev <62242809+DakshNamdev@users.noreply.github.com>
2021-11-30 15:25:01 +0530

    Update Branding, Console Logs, Added some info on console log

--- Events/ready.js
+++ Events/ready.js
@@ -2,7 +2,8 @@ const { REST } = require('@discordjs/rest'),
     { Routes } = require('discord-api-types/v9'),
     Discord = require('discord.js'),
     Utils = require('../Modules/Utils'),
-    packageJSON = require('../package.json')
+    packageJSON = require('../package.json'),
+    chalk = require('chalk')
 /**
  * 
  * @param {Discord.Client} bot 
@@ -10,9 +11,10 @@ const { REST } = require('@discordjs/rest'),
  */
 module.exports = async (bot) => {
     const { SlashCmds, SlashCmdsData, config } = bot;
+
     await Utils.logInfo('#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#');
     await Utils.logInfo('                                                                          ');
-    await Utils.logInfo(`                   • Zeltux ${packageJSON.version} is now Online! •       `);
+    await Utils.logInfo(`                    • Brayan Bot ${packageJSON.version} is now Online! •       `);
     await Utils.logInfo('                                                                          ');
     await Utils.logInfo('          • Join our Discord Server for any Issues/Custom Bots •          ');
     await Utils.logInfo('                     https://discord.gg/EgeZxGg6ev                        ');
@@ -21,15 +23,19 @@ module.exports = async (bot) => {
 
     await Utils.logInfo(`${bot.Commands.size} Command(s) Loaded.`)
     await Utils.logInfo(`${bot.Events.length} Event(s) Loaded.`)
-
     const rest = new REST({ version: '9' }).setToken(config.Settings.Token);
     try {
         await rest.put(Routes.applicationGuildCommands(bot.user.id, config.Settings.ServerID), {
             body: SlashCmdsData
         });
+        await Utils.logInfo(`${SlashCmdsData.length} Slash Command(s) Loaded.`)
     } catch (error) {
         Utils.logError(error);
     }
-    Utils.logInfo("Bot ready.")
+
+    await Utils.logInfo(`Logged in as: ${bot.user.tag}`)
+    bot.guilds.cache.size > 1 ? Utils.logWarning(`Currently in ${chalk.bold(bot.guilds.cache.size)} server(s). | ${chalk.hex("##ff596d")(`Brayan Bot is not made for multiple servers.`)}`) : Utils.logInfo(`Currently in ${chalk.bold(bot.guilds.cache.size)} server(s).`)
+    await Utils.logInfo(`Bot Ready!`);
+
 }
 module.exports.once = true
