99798240deba84c6bb20b8c9c167723820f6c825
DakshNamdev <62242809+DakshNamdev@users.noreply.github.com>
2021-11-30 13:55:46 +0530

    Update index.js
    Added AddonHandler to load

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('console-stamp')(console, { format: ':date(HH:MM:ss).bold.grey' });
-if(process.versions.node < 16.6) return require('./Modules/Utils').logError(`Zeltux Requires Node.js version 16.6.0 or Higher.`)
+if (process.versions.node < 16.6) return require('./Modules/Utils').logError(`Zeltux Requires Node.js version 16.6.0 or Higher.`)
 const fs = require('fs'),
     YAML = require('yaml'),
     Discord = require('discord.js'),
@@ -10,7 +10,7 @@ module.exports['client'] = client;
 ['config', 'lang', 'commands'].forEach(x => client[x] = module.exports[x]);
 ['Events', 'SlashCmds', 'SlashCmdsData'].forEach(x => client[x] = []);
 ['Commands', 'Aliases'].forEach(x => client[x] = new Discord.Collection());
-['ErrorHandler.js', 'EventHandler.js', 'CommandHandler.js'].forEach(file => require(`./Modules/Handlers/${file}`).init())
+['ErrorHandler.js', 'EventHandler.js', 'CommandHandler.js', 'AddonHandler.js'].forEach(file => require(`./Modules/Handlers/${file}`).init())
 
-if(client.config.Settings.Token) client.login(client.config.Settings.Token)
+if (client.config.Settings.Token) client.login(client.config.Settings.Token)
 else return Utils.logError("An invalid token was provided.")
