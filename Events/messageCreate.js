70da61d5a2fc6a637562211742a7fadf401ba236
DakshNamdev <62242809+DakshNamdev@users.noreply.github.com>
2021-12-02 12:28:31 +0530

    Update messageCreate.js

--- Events/messageCreate.js
+++ Events/messageCreate.js
@@ -5,43 +5,56 @@ const Discord = require("discord.js"),
 module.exports = async (bot, message) => {
     const { config, Commands, lang, Aliases } = bot;
 
-    if(message.content.startsWith(config.Settings.Prefix)) {
-        let msgArray = message.content.split(" "), command = msgArray[0].toLowerCase(), 
-            args = msgArray.slice(1), hasPermission = false
+    if (message.content.startsWith(config.Settings.Prefix) && typeof command.run == "function") {
+        let msgArray = message.content.split(" "),
+            command = msgArray[0].toLowerCase(),
+            args = msgArray.slice(1),
+            hasPermission = false
 
-        let commands = Commands.get(command.slice(config.Settings.Prefix.length)) 
+        let commands = Commands.get(command.slice(config.Settings.Prefix.length))
             || Commands.get(Aliases.get(command.slice(config.Settings.Prefix.length)))
-        if(commands) {
-            if(commands.commandData.Permission) {
-                if(typeof commands.commandData.Permission == 'string')  commands.commandData.Permission = [commands.Permission]
-                if(!commands.commandData.Permission[0]) commands.commandData.Permission[0] = "@everyone"
-                if(commands.commandData.Permission.includes("@everyone") || commands.commandData.Permission.includes("everyone")) {
+        if (commands) {
+            if (commands.commandData.Permission) {
+                if (typeof commands.commandData.Permission == 'string') {
+                    commands.commandData.Permission = [commands.Permission]
+                } else if (!commands.commandData.Permission[0]) {
+                    commands.commandData.Permission[0] = "@everyone"
+                }
+                if (commands.commandData.Permission.includes("@everyone") || commands.commandData.Permission.includes("everyone")) {
                     hasPermission = true
                 } else {
                     for (const role of commands.commandData.Permission) {
-                        if (!Utils.hasRole(message.member, role, true)) if(!hasPermission == true) hasPermission = false
-                        else hasPermission = true
+                        if (!Utils.hasRole(message.member, role, true)) {
+                            if (!hasPermission == true) hasPermission = false
+                        } else {
+                            hasPermission = true
+                        }
                     }
                 }
             }
-            if(hasPermission) commands.run(bot, message, args, config) 
-            else message.reply({ 
-                embeds: [
-                    Utils.setupEmbed({
-                        configPath: lang.Presets.NoPermission,
-                        variables: [
-                            { searchFor: /{roles}/g, replaceWith: commands.commandData.Permission.map(x => {
-                                let role = Utils.findRole(x, message.guild, true)
-                                if(role) return roleMention(role.id)
-                            }).join(", ")},
-                            ...Utils.userVariables(message.member)
-                        ]
-                    })
-                ], 
-                allowedMentions: { 
-                    repliedUser: false 
-                }
-            })
+            if (hasPermission) {
+                commands.run(bot, message, args, config)
+            } else {
+                message.reply({
+                    embeds: [
+                        Utils.setupEmbed({
+                            configPath: lang.Presets.NoPermission,
+                            variables: [
+                                {
+                                    searchFor: /{roles}/g, replaceWith: commands.commandData.Permission.map(x => {
+                                        let role = Utils.findRole(x, message.guild, true)
+                                        if (role) return roleMention(role.id)
+                                    }).join(", ")
+                                },
+                                ...Utils.userVariables(message.member)
+                            ]
+                        })
+                    ],
+                    allowedMentions: {
+                        repliedUser: false
+                    }
+                })
+            }
         }
     }
 }
