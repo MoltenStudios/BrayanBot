b4596ab186c1f72bba7850a5b50c7552fcc95d8a
Lloyd <37116855+LloydPlayzHD@users.noreply.github.com>
2021-12-02 23:59:17 +0000

    Update ErrorHandler.js

--- Modules/Handlers/ErrorHandler.js
+++ Modules/Handlers/ErrorHandler.js
@@ -4,8 +4,7 @@ const { client, config, lang, commands } = require("../../index"),
 
 module.exports = {
     init: () => {
-        /*process.on('uncaughtException', async (error) => {
-            Utils.logError(error)
+        process.on('uncaughtException', async (error) => {
         }).on('unhandledRejection', async (reason, promise) => {
             if(reason.toString() == "Error [TOKEN_INVALID]: An invalid token was provided.") {
                 Utils.logError("An invalid token was provided.")
@@ -14,14 +13,14 @@ module.exports = {
             const promiseText = require("util").inspect(promise) || "";
             const lines = "-=".repeat(reason.toString().length);
         
-            if(process.argv.includes('--show-errors')) {
+            if(process.argv.includes('--errors')) {
                 Utils.logError(`${reason.toString()}\n${promiseText}`)
             } 
             let data = `${lines}\n[[ Occured at: ${moment(Date.now()).format('MMMM Do YYYY, h:mm:ss a')} ]]\n${lines}\n${reason.toString()}\n${promiseText}\n${lines}\n`
             fs.appendFile("./errors.txt", data, (err) => {
                 if (err) Utils.logError(err);
-                Utils.logError(`An error occured, Contact ZLounge Support at https://zorino.in/discord`)
+                Utils.logError(`An error occured, You can view the error in errors.txt`)
             })
-        })*/
+        })
     }
 }
