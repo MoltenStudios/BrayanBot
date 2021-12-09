f801903284ce658e42e60393643dcb8f507064c3
59L <60123007+59L@users.noreply.github.com>
2021-12-09 16:38:43 -0500

    CreateCustomConfig.js For Utils

---
+++ Modules/Utils/createCustomConfig.js
@@ -0,0 +1,63 @@
+const fs = require("fs"),
+    YAML = require("yaml"),
+    { client, config, lang, commands } = require("../../index");
+
+/**
+ * 
+ * @param {componentsStructure} components 
+ * @param {variablesStructure} variables 
+ * @param {Boolean} isDisabled 
+ * @returns 
+ */
+module.exports = function (name, configData) {
+    let addon_config = null
+    const generateConfig = function (path, data, type, name) {
+        if (["yml", "yaml"].includes(type.toLowerCase())) {
+            fs.writeFileSync(path, YAML.stringify(data, { indent: 2, prettyErrors: true }))
+            addon_config = data
+        } else if (["json"].includes(type.toLowerCase())) {
+            fs.writeFileSync(path, JSON.stringify(data, null, 4))
+            addon_config = data
+        } else {
+            fs.writeFileSync(path, data)
+            addon_config = data
+        }
+
+        if (!fs.existsSync(path)) {
+            generateConfig(path, data, type, name)
+        }
+    }
+    const checkPath = function (path) {
+        const paths = path.split("/")
+        if (paths && paths.length >= 1) {
+            const dir = paths.slice(0, (paths.length - 1)).join("/")
+            if (!fs.existsSync(dir)) {
+                return fs.mkdir(dir, async (err) => {
+                    if (err) {
+                        return (err)
+                    }
+                    return true
+                })
+            } else return true
+        }
+    }
+    let { type, path, data } = configData;
+
+    checkPath(path)
+    if (fs.existsSync(path)) {
+        if (config.Settings.DevMode) {
+            generateConfig(path, data, type, name)
+        }
+        if (["yml"].includes(type.toLowerCase())) {
+            addon_config = YAML.parse(fs.readFileSync(path, 'utf-8'), { prettyErrors: true })
+        }
+        else if (["json"].includes(type.toLowerCase()))
+            addon_config = JSON.parse(fs.readFileSync(path, 'utf-8'))
+        else
+            addon_config = fs.readFileSync(path, 'utf-8')
+    } else {
+        generateConfig(path, data, type, name)
+    }
+    if (addon_config && addon_config !== null) return addon_config
+    else return undefined
+}
