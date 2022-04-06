9a737800bf41f5f9db17f0a5d92d520d9359a99b
SimonB50 <40821945+SimonB50@users.noreply.github.com>
2022-04-06 16:10:26 +0200

    Fix for Merge Conflicts

--- Events/messageCreate.js
+++ Events/messageCreate.js
@@ -2,6 +2,10 @@ const Discord = require("discord.js"),
     Utils = require("../Modules/Utils"),
     { roleMention, userMention } = require("@discordjs/builders");
 
+/**
+ * @param {Discord.Client} bot
+ * @param {Discord.Message} message
+ */
 module.exports = async (bot, message) => {
     const { config, Commands, lang, Aliases } = bot;
 
@@ -14,89 +18,71 @@ module.exports = async (bot, message) => {
 
         if (commands && typeof commands.run == "function") {
             if (commands.commandData.AllowedChannels) {
-                if (typeof commands.commandData.AllowedChannels == "string") {
+                if (typeof commands.commandData.AllowedChannels == "string")
                     commands.commandData.AllowedChannels = [commands.commandData.AllowedChannels];
-                }
-                var isAllowed = [],
-                    ids = [];
-                for (var index = 0; index < commands.commandData.AllowedChannels.length; index++) {
-                    var ch = Utils.findChannel(commands.commandData.AllowedChannels[index], message.guild, "GUILD_TEXT", true)
-                    if (ch) {
-                        ids.push(`<#${ch.id}>`)
-                        if (message.channel.id == ch.id) {
-                            isAllowed.push(true)
-                        }
+                else if (!commands.commandData.AllowedChannels[0]) commands.commandData.AllowedChannels = [];
+
+                let isAllowed = [], channelIDs = [];
+                commands.commandData.AllowedChannels.forEach(channel => {
+                    const allowedChannel = Utils.findChannel(channel, message.guild, "GUILD_TEXT", true)
+                    if (allowedChannel) {
+                        if (message.channel.id == allowedChannel.id) isAllowed.push(true)
+                        channelIDs.push(`<#${allowedChannel.id}>`)
                     }
-                }
-                if (!isAllowed.includes(true)) {
-                    return message.reply(Utils.setupMessage({
-                        configPath: lang.Presets.NonCommandChannel,
-                        variables: [
-                            { searchFor: /{channels}/g, replaceWith: ids.join(", ") },
-                            ...Utils.userVariables(message.member),
-                        ],
-                    })).then(x => {
-                        setTimeout(() => {
-                            x.delete().catch(e => { })
-                            message.delete().catch(e => { })
-                        }, 5000)
-                    })
-                }
-            }
-            if (commands.commandData.Permission) {
-                if (typeof commands.commandData.Permission == "string") {
-                    commands.commandData.Permission = [commands.commandData.Permission];
-                } else if (!commands.commandData.Permission[0]) {
-                    commands.commandData.Permission = [];
-                    commands.commandData.Permission.push("@everyone");
-                }
-                if (commands.commandData.Permission.includes("@everyone") || commands.commandData.Permission.includes("everyone")) {
-                    permissions.push(true);
-                } else {
-                    for (const perm of commands.commandData.Permission) {
-                        const prole = !!Utils.findRole(perm, message.guild, false),
-                            puser = !!Utils.parseUser(perm, message.guild, false);
-                        if (!prole && !puser) return Utils.logError(`${chalk.bold(perm)} role nor user was not found in ${chalk.bold(message.guild.name)} guild`);
-                        if (prole) {
-                            if (Utils.hasRole(message.member, perm, true)) {
-                                permissions.push(true);
-                            } else {
-                                permissions.push(false);
-                            }
-                        }
-                        if (puser) {
-                            const pmember = Utils.parseUser(perm, message.guild, true)
-                            if (message.member == pmember.id) {
-                                permissions.push(true)
-                            } else {
-                                permissions.push(false)
-                            }
-                        }
-                    }
-                }
-            }
-            if (permissions.includes(true)) {
-                commands.run(bot, message, args, config);
-            } else {
-                message.reply(Utils.setupMessage({
-                    configPath: lang.Presets.NoPermission,
+                });
+
+                if (!isAllowed.includes(true)) return message.reply(Utils.setupMessage({
+                    configPath: lang.Presets.NonCommandChannel,
                     variables: [
-                        {
-                            searchFor: /{perms}/g, replaceWith: commands.commandData.Permission.map((x) => {
-                                if (!!Utils.findRole(x, message.guild, false)) {
-                                    let role = Utils.findRole(x, message.guild, true);
-                                    return roleMention(role.id);
-                                }
-                                if (!!Utils.parseUser(x, message.guild, false)) {
-                                    let user = Utils.parseUser(x, message.guild, true);
-                                    return userMention(user.id);
-                                }
-                            }).join(", "),
-                        },
+                        { searchFor: /{channels}/g, replaceWith: channelIDs.join(", ") },
                         ...Utils.userVariables(message.member),
                     ],
-                }));
+                })).then(msg => setTimeout(() => {
+                    message.delete().catch(e => { })
+                    msg.delete().catch(e => { })
+                }, 5000))
             }
+            if (commands.commandData.Permission) {
+                if (typeof commands.commandData.Permission == "string")
+                    commands.commandData.Permission = [commands.commandData.Permission];
+                    else if (!commands.commandData.Permission[0]) commands.commandData.Permission = ["@everyone"];
+
+                    if (commands.commandData.Permission.includes("@everyone") || commands.commandData.Permission.includes("everyone"))
+                    permissions.push(true);
+                    else commands.commandData.Permission.forEach(permission => {
+                        const permissionRole = !!Utils.findRole(permission, message.guild, false),
+                            permissionUser = !!Utils.parseUser(permission, message.guild, false);
+    
+                        if (!permissionRole && !permissionUser)
+                            return Utils.logError(`${chalk.bold(perm)} role/user was not found in ${chalk.bold(message.guild.name)} guild`);
+    
+                        if (permissionRole) Utils.hasRole(message.member, permission, true)
+                            ? permissions.push(true) : permissions.push(false);
+    
+                        if (permissionUser) {
+                            const parsedUser = Utils.parseUser(permission, message.guild, true)
+                            parsedUser && message.member == parsedUser.id
+                                ? permissions.push(true) : permissions.push(false);
+                    }
+                })
+            }
+            if (permissions.includes(true)) {
+                await commands.run(bot, message, args, config);
+                if (commands.commandData.DeleteCommand) message.delete().catch(e => { })
+            } else message.reply(Utils.setupMessage({
+                configPath: lang.Presets.NoPermission,
+                variables: [
+                    ...Utils.userVariables(message.member),
+                    {
+                        searchFor: /{roles}/g, replaceWith: commands.commandData.Permission.map((x) => {
+                            if (!!Utils.findRole(x, message.guild, false)) {
+                                let role = Utils.findRole(x, message.guild, true);
+                                return roleMention(role.id);
+                            }
+                        }).join(", "),
+                    },
+                ],
+            }));
         }
     }
 };
