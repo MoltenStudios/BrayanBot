3634f5ca451d5cb65070b66470cfe00314795209
Phantom <73062410+Phantom-codes@users.noreply.github.com>
2022-06-04 10:51:58 +0530

    Add files via upload

--- Commands/Admin/eval.js
+++ Commands/Admin/eval.js
@@ -1,11 +1,12 @@
 const Utils = require("../../Modules/Utils"),
-    { lang, config, commands } = require("../../index"),
-    moment = require("moment");
+  { lang, config, commands } = require("../../index"),
+  { post } = require("node-superfetch");
+moment = require("moment");
 
 module.exports = {
-    name: "eval",
-    type: "admin",
-    commandData: commands.Admin.Eval,
+  name: "eval",
+  type: "admin",
+  commandData: commands.Admin.Eval,
 };
 
 /**
@@ -15,51 +16,93 @@ module.exports = {
  * @param {Array} args
  */
 module.exports.run = async (bot, message, args) => {
-    // Define code to eval
-    const input = args.join(" ");
-    // Send error message if code is empty
-    if (!input) return message.reply(Utils.setupMessage({
+  // Define code to eval
+  const input = args.join(" ");
+  // Send error message if code is empty
+  if (!input)
+    return message.reply(
+      Utils.setupMessage({
         configPath: lang.Presets.Error,
         variables: [
-            { searchFor: /{error}/g, replaceWith: "You need to define code, which should be executed!" },
-            ...Utils.userVariables(message.member),
-            ...Utils.botVariables(bot),
+          {
+            searchFor: /{error}/g,
+            replaceWith: "You need to define code, which should be executed!",
+          },
+          ...Utils.userVariables(message.member),
+          ...Utils.botVariables(bot),
         ],
-    }));
-    // Send error message if input includes bot.token
-    if (input.includes("token")) return message.reply(Utils.setupMessage({
+      })
+    );
+  // Send error message if input includes bot.token
+  if (input.includes("token"))
+    return message.reply(
+      Utils.setupMessage({
         configPath: lang.Presets.Error,
         variables: [
-            { searchFor: /{error}/g, replaceWith: "You can't execute code, which includes bot token!" },
-            ...Utils.userVariables(message.member),
-            ...Utils.botVariables(bot),
+          {
+            searchFor: /{error}/g,
+            replaceWith: "You can't execute code, which includes bot token!",
+          },
+          ...Utils.userVariables(message.member),
+          ...Utils.botVariables(bot),
         ],
-    }));
-    // Try to execute code
-    try {
-        var output = await eval(input);
-    } catch (e) {
-        var output = e;
-    }
-    // Send error message if output includes token
-    if (typeof output != "map" && output.toString().includes(bot.token)) return message.reply(Utils.setupMessage({
+      })
+    );
+  // Try to execute code
+  let clr;
+  try {
+    var output = `\`\`\`js\n${await eval(input)}\n\`\`\``;
+    clr = "#3c70dc";
+  } catch (e) {
+    var output = `\`\`\`js\n${e}\n\`\`\``;
+    clr = "RED";
+  }
+  // Send error message if output includes token
+  if (typeof output != "map" && output.toString().includes(bot.token))
+    return message.reply(
+      Utils.setupMessage({
         configPath: lang.Presets.Error,
         variables: [
-            { searchFor: /{error}/g, replaceWith: "You can't execute code, which includes bot token!" },
-            ...Utils.userVariables(message.member),
-            ...Utils.botVariables(bot),
+          {
+            searchFor: /{error}/g,
+            replaceWith: "You can't execute code, which includes bot token!",
+          },
+          ...Utils.userVariables(message.member),
+          ...Utils.botVariables(bot),
         ],
-    }));
-    // Send message with eval output
-    message.reply(Utils.setupMessage({
+      })
+    );
+  if (output.length > 1024) {
+    const { body } = await post("https://hastebin.com/documents").send(output);
+    let output_link = `https://hastebin.com/${body.key}.js`;
+    message.reply(
+      Utils.setupMessage({
         configPath: lang.Admin.Eval,
         variables: [
-            { searchFor: /{input}/g, replaceWith: input },
-            { searchFor: /{output}/g, replaceWith: output },
-            ...Utils.userVariables(message.member),
-            ...Utils.botVariables(bot),
+          { searchFor: /{input}/g, replaceWith: input },
+          { searchFor: /{output}/g, replaceWith: output_link },
+          { searchFor: /{color}/g, replaceWith: clr },
+          ...Utils.userVariables(message.member),
+          ...Utils.botVariables(bot),
         ],
-    }));
+      })
+    );
+    return;
+  }
+
+  // Send message with eval output
+  message.reply(
+    Utils.setupMessage({
+      configPath: lang.Admin.Eval,
+      variables: [
+        { searchFor: /{input}/g, replaceWith: input },
+        { searchFor: /{output}/g, replaceWith: output },
+        { searchFor: /{color}/g, replaceWith: clr },
+        ...Utils.userVariables(message.member),
+        ...Utils.botVariables(bot),
+      ],
+    })
+  );
 };
 /**
  *
@@ -67,49 +110,90 @@ module.exports.run = async (bot, message, args) => {
  * @param {Discord.Interaction} interaction
  */
 module.exports.runSlash = async (bot, interaction) => {
-    // Define code to eval
-    const input = interaction.options.getString("code");
-    // Send error message if code is empty
-    if (!input) return interaction.reply(Utils.setupMessage({
+  // Define code to eval
+  const input = interaction.options.getString("code");
+  // Send error message if code is empty
+  if (!input)
+    return interaction.reply(
+      Utils.setupMessage({
         configPath: lang.Presets.Error,
         variables: [
-            { searchFor: /{error}/g, replaceWith: "You need to define code, which should be executed!" },
-            ...Utils.userVariables(interaction.member),
-            ...Utils.botVariables(bot),
+          {
+            searchFor: /{error}/g,
+            replaceWith: "You need to define code, which should be executed!",
+          },
+          ...Utils.userVariables(interaction.member),
+          ...Utils.botVariables(bot),
         ],
-    }));
-    // Send error message if input includes bot.token
-    if (input.includes("bot.token")) return interaction.reply(Utils.setupMessage({
+      })
+    );
+  // Send error message if input includes bot.token
+  if (input.includes("bot.token"))
+    return interaction.reply(
+      Utils.setupMessage({
         configPath: lang.Presets.Error,
         variables: [
-            { searchFor: /{error}/g, replaceWith: "You can't execute code, which includes bot token!" },
-            ...Utils.userVariables(interaction.member),
-            ...Utils.botVariables(bot),
+          {
+            searchFor: /{error}/g,
+            replaceWith: "You can't execute code, which includes bot token!",
+          },
+          ...Utils.userVariables(interaction.member),
+          ...Utils.botVariables(bot),
         ],
-    }));
-    // Try to execute code
-    try {
-        var output = await eval(input);
-    } catch (e) {
-        var output = e;
-    }
-    // Send error message if output includes token
-    if (typeof output != "map" && output.toString().includes(bot.token)) return interaction.reply(Utils.setupMessage({
+      })
+    );
+  // Try to execute code
+  let clr;
+  try {
+    var output = `\`\`\`js\n${await eval(input)}\n\`\`\``;
+    clr = "#3c70dc";
+  } catch (e) {
+    var output = `\`\`\`js\n${e}\n\`\`\``;
+    clr = "RED";
+  }
+  // Send error message if output includes token
+  if (typeof output != "map" && output.toString().includes(bot.token))
+    return interaction.reply(
+      Utils.setupMessage({
         configPath: lang.Presets.Error,
         variables: [
-            { searchFor: /{error}/g, replaceWith: "You can't execute code, which includes bot token!" },
-            ...Utils.userVariables(interaction.member),
-            ...Utils.botVariables(bot),
+          {
+            searchFor: /{error}/g,
+            replaceWith: "You can't execute code, which includes bot token!",
+          },
+          ...Utils.userVariables(interaction.member),
+          ...Utils.botVariables(bot),
         ],
-    }));
-    // Send message with eval output
-    interaction.reply(Utils.setupMessage({
+      })
+    );
+  if (output.length > 1024) {
+    const { body } = await post("https://hastebin.com/documents").send(output);
+    let output_link = `https://hastebin.com/${body.key}.js`;
+    interaction.reply(
+      Utils.setupMessage({
         configPath: lang.Admin.Eval,
         variables: [
-            { searchFor: /{input}/g, replaceWith: input },
-            { searchFor: /{output}/g, replaceWith: output },
-            ...Utils.userVariables(interaction.member),
-            ...Utils.botVariables(bot),
+          { searchFor: /{input}/g, replaceWith: input },
+          { searchFor: /{output}/g, replaceWith: output_link },
+          { searchFor: /{color}/g, replaceWith: clr },
+          ...Utils.userVariables(interaction.member),
+          ...Utils.botVariables(bot),
         ],
-    }));
+      })
+    );
+    return;
+  }
+  // Send message with eval output
+  interaction.reply(
+    Utils.setupMessage({
+      configPath: lang.Admin.Eval,
+      variables: [
+        { searchFor: /{input}/g, replaceWith: input },
+        { searchFor: /{output}/g, replaceWith: output },
+        { searchFor: /{color}/g, replaceWith: clr },
+        ...Utils.userVariables(interaction.member),
+        ...Utils.botVariables(bot),
+      ],
+    })
+  );
 };
