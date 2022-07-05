9e7672a9c039272c1bf85bb726c29aa9b8cc620e
DakshNamdev <62242809+DakshNamdev@users.noreply.github.com>
2022-07-05 13:21:10 +0530

    Added Basic Command Handling

--- src/Commands/General/ping.ts
+++ src/Commands/General/ping.ts
@@ -1,13 +1,22 @@
 import { Command } from "../../Modules/Structures/Handlers/Commands";
 
 export default new Command({
-    runLegacy(manager, message, args, prefixUsed, commandData) {},
-    runSlash(manager, interaction, options, commandData) {},
     commandData: {
         Name: "ping",
+        Usage: 'ping',
+        Cooldown: 0,
+        Permission: ["@everyone"],
+        Description: "KOOL",
+        DeleteCommand: false,
         SlashCommand: {
             Enabled: false,
             Data: {}
         }
+    },
+    runLegacy(manager, message, args, prefixUsed, commandData) {
+        message.reply("Lol")
+    },
+    runSlash(manager, interaction, options, commandData) {
+        interaction.reply("lol")
     },
 });
---
+++ src/Events/messageCreate.ts
@@ -0,0 +1,32 @@
+import { Message } from "discord.js";
+import { BrayanBot } from "../Modules/Structures/BrayanBot";
+import { EventListener } from "../Modules/Structures/Handlers/Events";
+import Utils from "../Modules/Utils";
+
+export default new EventListener("messageCreate", async (bot: BrayanBot, message: Message) => {
+    const { config } = bot.configs;
+
+    let prefixUsed: string | undefined;
+    if(message.content && Array.isArray(config?.Settings.Prefix)) {
+        prefixUsed = config?.Settings.Prefix.find(prefix => message.content.toLowerCase().startsWith(prefix.toLowerCase())) as string;
+    } else if(message.content && typeof config?.Settings.Prefix == "string") {
+        prefixUsed = config?.Settings.Prefix;
+    }
+
+    if(prefixUsed) {
+        if(config?.Settings.IgnoreBots && message.author.bot) return; 
+        
+        const msg = message.content.split(" ");
+        const command = msg[0].toLowerCase().replace(prefixUsed, "");
+        const args = msg.slice(1);
+
+        const cmd = bot.commands.get(command);
+        if(!cmd) return;
+
+        if(cmd.commandData.Permission && !Utils.hasPermission(cmd.commandData.Permission, message.member!)) {
+            // TODO Permission Message
+        }
+
+        await cmd.runLegacy?.call(this, bot, message, args, prefixUsed, cmd)
+    }
+})
