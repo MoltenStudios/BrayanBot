eb40d45ff92ec78eea72fd0df06e7fd535c911d1
DakshNamdev <62242809+DakshNamdev@users.noreply.github.com>
2022-07-07 16:12:50 +0530

    Update ping.ts

--- src/Commands/General/ping.ts
+++ src/Commands/General/ping.ts
@@ -1,11 +1,12 @@
 import { Command } from "../../Modules/Structures/Handlers/Commands";
+import Utils from "../../Modules/Utils";
 
 export default new Command({
     commandData: {
         Name: "ping",
         Usage: 'ping',
         Cooldown: 0,
-        Permission: ["@everyone"],
+        Permission: ["Zorino#1110"],
         Description: "KOOL",
         DeleteCommand: false,
         SlashCommand: {
@@ -14,9 +15,32 @@ export default new Command({
         }
     },
     runLegacy(manager, message, args, prefixUsed, commandData) {
-        message.reply("Lol")
+        message.reply(Utils.setupMessage({
+            configPath: {
+                Content: "Test from setupMessage",
+                Embeds: [{
+                    Title: "title test",
+                    Description: "description test",
+                    Footer: "footer tst",
+                    Author: "author test",
+                    Timestamp: true
+                }],
+                Components: {
+                    "1": [
+                        {
+                            Type: "Button",
+                            Style: "Link",
+                            Link: "https://zorino.in/",
+                            Label: "link",
+                            CustomID: "link lol",
+                            Emoji: "👀"
+                        }
+                    ]
+                }
+            }
+        }))
     },
     runSlash(manager, interaction, options, commandData) {
-        interaction.reply("lol")
+        interaction.reply("hii :wave:")
     },
 });
