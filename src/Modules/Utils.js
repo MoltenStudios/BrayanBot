a18272f927a252db67c7ac3a9844015da52112bf
Daksh Namdev <62242809+DakshNamdev@users.noreply.github.com>
2023-01-20 17:13:49 +0530

    Update Utils.js

--- src/Modules/Utils.js
+++ src/Modules/Utils.js
@@ -1,38 +1,49 @@
-import { ChannelType, TextChannel, VoiceChannel, CategoryChannel, DMChannel } from "discord.js";
+import { ChannelType, TextChannel, VoiceChannel, CategoryChannel, DMChannel, Guild, Role, GuildMember } from "discord.js";
+import { botVariables, channelVariables, guildVariables, roleVariables, userVariables } from "./Variables.js"
 import { setupSlashCommand } from "./Utils/setupSlashCommand.js";
 import { setupMessage } from "./Utils/setupMessage.js";
 import { loadCommands } from "./Utils/loadCommands.js";
-import { botVariables, channelVariables, guildVariables, roleVariables, userVariables } from "./Variables.js"
 import chalk from "chalk";
 
-export default class Utils {
-    static logger = {
+export default {
+    logger: {
         debug: (...text) => console.log(chalk.magentaBright.bold("[DEBUG]"), ...text),
         info: (...text) => console.log(chalk.greenBright.bold("[INFO]"), ...text),
         warn: (...text) => console.log(chalk.yellowBright.bold("[WARN]"), ...text),
         error: (...text) => console.log(chalk.redBright.bold("[ERROR]"), ...text),
-    }
+    },
 
-    static loadCommands = loadCommands; static setupMessage = setupMessage; static setupSlashCommand = setupSlashCommand;
-    static botVariables = botVariables; static userVariables = userVariables;
-    static roleVariables = roleVariables; static guildVariables = guildVariables;
-    static channelVariables = channelVariables;
+    loadCommands: loadCommands,
+    setupMessage: setupMessage,
+    setupSlashCommand: setupSlashCommand,
 
-    static getRandom(array) {
-        return array[Math.floor(Math.random() * array.length)];
-    }
+    botVariables: botVariables,
+    userVariables: userVariables,
+    roleVariables: roleVariables,
+    guildVariables: guildVariables,
+    channelVariables: channelVariables,
 
-    static applyVariables(string, variables) {
+    /** @param {Array} array */
+    getRandom: (array) => array[Math.floor(Math.random() * array.length)],
+
+    /** @param {string} string @param {[{ searchFor: RegExp, replaceWith: any }]} variables*/
+    applyVariable: (string, variables) => {
         return variables.reduce((output, variable) => output.replace(variable.searchFor, variable.replaceWith), string);
-    }
+    },
 
-    static paginateArray(array, itemsperpage, page = 1) {
+    /** @param {Array} array @param {number} itemsperpage @param {number} page */
+    paginateArray: (array, itemsperpage, page = 1) => {
         const maxPages = Math.ceil(array.length / itemsperpage);
         if (page < 1 || page > maxPages) return null;
         return array.slice((page - 1) * itemsperpage, page * itemsperpage)
-    }
-
-    static findRole(guild, name, notify = true) {
+    },
+    /**
+     * @param {Guild} guild 
+     * @param {String} name 
+     * @param {Boolean} notify 
+     * @returns {Role | undefined}
+     */
+    findRole: (guild, name, notify = true) => {
         const roleName = typeof name === "number" ? name.toString() : name;
         const role = guild.roles.cache.find((r) => {
             return r.name.toLowerCase() === roleName.toLowerCase() || r.id === roleName.toLowerCase();
@@ -43,9 +54,14 @@ export default class Utils {
         }
 
         return role;
-    }
-
-    static findMember(guild, name, notify = false) {
+    },
+    /**
+     * @param {Guild} guild 
+     * @param {String} name 
+     * @param {Boolean} notify 
+     * @returns {GuildMember | undefined}
+     */
+    findMember: (guild, name, notify = false) => {
         const memberName = typeof name === "number" ? name.toString() : name;
         const member = guild.members.cache.find((m) => {
             return m.user.tag.toLowerCase() === memberName.toLowerCase() || m.id.toLowerCase() === memberName.toLowerCase() || m.user.username.toLowerCase() === memberName.toLowerCase();
@@ -56,9 +72,16 @@ export default class Utils {
         }
 
         return member;
-    }
+    },
 
-    static findChannel(type, guild, name, notify = false) {
+    /**
+     * @param {ChannelType} type 
+     * @param {Guild} guild 
+     * @param {String} name 
+     * @param {Boolean} notify 
+     * @returns {TextChannel | VoiceChannel | DMChannel | CategoryChannel | undefined}
+     */
+    findChannel: (type, guild, name, notify = false) => {
         if (typeof name === "number") name = name.toString();
         const channels = guild.channels.cache.filter(channel => channel.type === type);
         const channel = channels.find(ch => ch.name === name || ch.id === name);
@@ -77,10 +100,13 @@ export default class Utils {
             default:
                 if (notify) this.logger.error(`The ${chalk.bold(ChannelType[type].toString())} channel named "${chalk.bold(name)}" could not be found in the "${chalk.bold(guild.name)}" server. Please check the channel name and its type and try again.`);
         }
-    }
-
-
-    static hasPermission(permissions, member) {
+    },
+    /**
+     * @param {[String]} permissions Array of role names or IDs
+     * @param {GuildMember} member 
+     * @returns {Boolean}
+     */
+    hasPermission: (permissions, member) => {
         if (!Array.isArray(permissions)) permissions = [permissions];
         return permissions.some((perm) => {
             const isRolePermission = this.findRole(member.guild, perm, false);
@@ -89,9 +115,9 @@ export default class Utils {
             const isMemberPermission = this.findMember(member.guild, perm, false);
             if (isMemberPermission && member.id == isMemberPermission.id) return true;
         })
-    }
-
-    static getUserBadges(member) {
+    },
+    /** @param {GuildMember} member */
+    getUserBadges: (member) => {
         const badges = {
             BugHunterLevel1: "Discord Bug Hunter Level 1",
             BugHunterLevel2: "Discord Bug Hunter Level 2",
@@ -113,9 +139,9 @@ export default class Utils {
         return Object.entries(badges)
             .filter(([badge, _]) => member.user.flags?.has(badge))
             .map(([_, value]) => value);
-    }
+    },
 
-    static getUserFromMessage(message, arg = 0, checkFull = false) {
+    getUserFromMessage: (message, arg = 0, checkFull = false) => {
         const args = message.content.split(" "); args.shift();
         const toFind = checkFull ? args.join(" ") : (args[arg] || '');
 
@@ -126,5 +152,5 @@ export default class Utils {
                 || member.user.username.toLowerCase() == toFind.toLowerCase()
                 || member.id == toFind.replace(/([<@!]|[>])/g, "")
             );
-    }
+    },
 }
